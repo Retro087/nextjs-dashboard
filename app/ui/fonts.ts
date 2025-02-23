4177c0d368317bc3513ebc4b117295f3080354b2
Retro087 <vovabsjkpv8@gmail.com>
2025-02-23 17:48:59 +0300

    fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,6 @@ import {Inter} from 'next/font/google'
 export const inter = Inter({subsets: ['latin']})
 
 import { Lusitana } from 'next/font/google'
-export const lusitana = Lusitana({weight: '700'})
+export const lusitana = Lusitana({weight: '700',
+    subsets: ['latin']
+})
